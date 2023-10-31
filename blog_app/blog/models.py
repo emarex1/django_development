b3b606df73008944b9a01293316d5750d7af1652
emarex1 <emmanuelawoh0@gmail.com>
2023-10-31 19:37:00 +0100

    create file

--- blog_app/blog/models.py
+++ blog_app/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,3 +9,7 @@ class Post(models.Model):
     def __str__(self):
         return (self.title)
 
+    #def __str__(self):
+        #return (self.body[:80])
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
